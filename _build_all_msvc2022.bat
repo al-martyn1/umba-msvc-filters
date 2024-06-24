b7a42ee5d8d79c2d90d3349a6727e8dbb02a0973
al-martyn1 <amart@mail.ru>
2024-06-25 02:52:08 +0300

    Добавил поддержку 2022ой студии

---
+++ _build_all_msvc2022.bat
@@ -0,0 +1,5 @@
+@call ~call_cmake_msvc_impl.bat msvc2022 x86 GENERATE
+@call ~call_cmake_msvc_impl.bat msvc2022 x64 GENERATE
+@rem 
+@call ~call_cmake_msvc_impl.bat msvc2022 x86 BUILD
+@call ~call_cmake_msvc_impl.bat msvc2022 x64 BUILD
---
+++ _start_msvc2022_x86.bat
@@ -0,0 +1,2 @@
+@call _cmake_generate_msvc2022.bat
+@start "" ".out\msvc2022\x86\umba-msvc-filters.sln"
